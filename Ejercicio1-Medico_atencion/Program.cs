6764bb96fcaa22f7ebdf985e33472195c2d6f5ec
Gerber Ramírez <koala57777@gmail.com>
2026-02-21 13:20:03 -0600

    Válidación de datos.

--- Ejercicio1-Medico_atencion/Program.cs
+++ Ejercicio1-Medico_atencion/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("");
 Console.WriteLine("Ingrese la edad del paciente: ");
 int edad=int.Parse(Console.ReadLine());
 Console.WriteLine("");
-Console.WriteLine("Ingrese la temperatura del paciente: ");
+Console.WriteLine("Ingrese la temperatura del paciente (℃): ");
 double temperatura = double.Parse(Console.ReadLine());
 Console.WriteLine("");
 Console.WriteLine("Ingrese la saturación de oxígeno del paciente (%): ");
@@ -19,9 +19,16 @@ Console.WriteLine("");
 Console.WriteLine("Ingrese el nivel de dolor del paciente (0-10): ");
 int niveldolor = int.Parse(Console.ReadLine());
 Console.WriteLine("");
-Console.WriteLine("Ingrese la presión sistólica del paciente: ");
+Console.WriteLine("Ingrese la presión sistólica del paciente (mmHg): ");
 int presionsistolica = int.Parse(Console.ReadLine());
 Console.WriteLine("");
 
+if (edad >= 0 && presionsistolica >=0 && presionsistolica<=200 && niveldolor >= 0 && niveldolor<=10&& temperatura >= 0 && temperatura<=50 && saturacionoxigeno >= 0 && saturacionoxigeno<=100)
+{
 
+}
+else
+{
+    Console.WriteLine("Datos ingresados no válidos. Por favor, ingrese valores válidos.");
+}
 
