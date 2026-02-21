399e509edb326e89d661102f3e9959ffdc945756
Gerber Ramírez <koala57777@gmail.com>
2026-02-21 13:09:28 -0600

    Se implementó el menú y datos de entrada.

--- Ejercicio1-Medico_atencion/Program.cs
+++ Ejercicio1-Medico_atencion/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Console.WriteLine("Ingrese el tipo de atención requerida (Número): ");
+Console.WriteLine("");
+Console.WriteLine("1. Emergencia");
+Console.WriteLine("2. Consulta");
+Console.WriteLine("3. Pediatría");
+Console.WriteLine("4. Traumatología");
+Console.WriteLine("");
+int atencion=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese la edad del paciente: ");
+int edad=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese la temperatura del paciente: ");
+double temperatura = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese la saturación de oxígeno del paciente (%): ");
+double saturacionoxigeno = double.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese el nivel de dolor del paciente (0-10): ");
+int niveldolor = int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese la presión sistólica del paciente: ");
+int presionsistolica = int.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+
+
